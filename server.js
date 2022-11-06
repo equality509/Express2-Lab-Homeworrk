924e6e56bec854c83aaf5217b8d38394a5601aa0
Eric Turner <equality509@gmail.com>
2022-11-06 09:47:48 -0500

    Greeting express application created

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+
+
+
+
+app.get('/greeting/:name', (req, res) => {
+    res.send(`Hello, stranger ${req.params.name}`)
+})
+
+
+app.listen(3000, () => {
+    console.log('listening 3000')
+});
+
