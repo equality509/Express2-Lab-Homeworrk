c45b43aab04b7d479879550c9f891b8b2f93db6d
Eric Turner <equality509@gmail.com>
2022-11-06 11:16:57 -0500

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Hello, stranger ${req.params.name}`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Your tipwill be ${parseInt(req.params.total) / parseInt(req.params.tipPercentage)}`)
+})
+
 
 app.listen(3000, () => {
     console.log('listening 3000')
